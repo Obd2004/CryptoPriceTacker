904eb63a7ca3e9599f9e327a677e4b3e6b6a717e
Obd2004 <obidjonabd1097@gmail.com>
2023-03-27 14:51:38 +0500

    my CryptoPriceTacker

---
+++ src/priceTracker/Logo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Col, Container, Row } from 'reactstrap'
+import logo from '../priceTracker/images/Logo.png'
+function Logo() {
+  return (
+    <>
+        <Container>
+            <Row>
+                <Col md='12' className='text-center mt-5'>
+                    <img src={logo} alt="" />
+                </Col>
+            </Row>
+        </Container>
+    </>
+  )
+}
+
+export default Logo
